7ad69f7d549d1a1d0f732fa384ed1e4d43e37790
Mohsen Mojaddam محسن مجدم <56688766+mohsenmojadam2019@users.noreply.github.com>
2026-08-31 02:49:17 +0330

    Seed the complete notification catalog idempotently

--- database/seeders/EmailSettingSeeder.php
+++ database/seeders/EmailSettingSeeder.php
@@ -4,28 +4,15 @@ namespace Database\Seeders;
 
 use App\Models\EmailNotificationSetting;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
 
 class EmailSettingSeeder extends Seeder
 {
     public function run(): void
     {
-        $notificationSettings = [
-            ['setting_name' => 'User Registration/Added by Admin', 'send_email' => 'yes'],
-            ['setting_name' => 'Employee Assign to Project', 'send_email' => 'yes'],
-            ['setting_name' => 'New Notice Published', 'send_email' => 'no'],
-            ['setting_name' => 'User Assign to Task', 'send_email' => 'yes'],
-        ];
-
-        foreach ($notificationSettings as $setting) {
-            $slug = Str::slug($setting['setting_name']);
-
+        foreach (EmailNotificationSetting::NOTIFICATIONS as $setting) {
             EmailNotificationSetting::query()->updateOrCreate(
-                ['slug' => $slug],
-                [
-                    'setting_name' => $setting['setting_name'],
-                    'send_email' => $setting['send_email'],
-                ]
+                ['slug' => $setting['slug']],
+                $setting
             );
         }
     }
